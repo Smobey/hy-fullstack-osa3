d28878e4a762c590e6d95e03bc05cbfaff976637
Topi T <smobey@gmail.com>
2018-02-09 13:48:21 +0200

    3.12

---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://smobey:(pass)@ds229438.mlab.com:29438/puhelinluettelo-db'
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+    name: String,
+    number: String,
+})
+
+if (process.argv.length < 3) {
+    Person
+    .find({})
+    .then(result => {
+        console.log("puhelinluettelo:")
+        result.forEach(person => {
+        console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+
+else {
+    const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
+    })
+    
+    person
+    .save()
+    .then(response => {
+        console.log("lisätään henkilö " + process.argv[2] + " numero " + process.argv[3] + " luetteloon")
+        mongoose.connection.close()
+    })
+}
+
